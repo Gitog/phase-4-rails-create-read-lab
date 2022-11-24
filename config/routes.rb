23e600c68b019a7452c1ef06a59437ffa420c969
James <jamesnjorio@gmail.com>
2022-11-24 04:59:37 +0300

    lab complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlge
+  # get '/plants', to: 'plants#index'
+  # get '/plants/:id', to: 'plants#show'
+  resources :plants, only: [:index, :show, :create]
+
 end
